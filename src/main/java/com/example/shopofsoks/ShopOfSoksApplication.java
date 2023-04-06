3e72657febbb486b901f39287ef83237e239a197
abdullinru <abdullin17021989@gmail.com>
2023-04-06 15:37:51 +0600

    Initial commit

---
+++ src/main/java/com/example/shopofsoks/ShopOfSoksApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopofsoks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopOfSoksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopOfSoksApplication.class, args);
+    }
+
+}
