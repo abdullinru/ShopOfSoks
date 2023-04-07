57abd6e47c61983d5d7ad38849b7880ed7d74b76
abdullinru <abdullin17021989@gmail.com>
2023-04-07 15:28:37 +0600

    Создал класс SockDto и настроил его маппинг на классы Sock и SockCount

---
+++ src/main/java/com/example/shopofsoks/dto/SockDto.java
@@ -0,0 +1,31 @@
+package com.example.shopofsoks.dto;
+
+public class SockDto {
+    private String color;
+    private int cottonPart;
+    private int quantity;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getCottonPart() {
+        return cottonPart;
+    }
+
+    public void setCottonPart(int cottonPart) {
+        this.cottonPart = cottonPart;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
---
+++ src/main/java/com/example/shopofsoks/mapper/SockMapper.java
@@ -0,0 +1,21 @@
+package com.example.shopofsoks.mapper;
+
+import com.example.shopofsoks.dto.SockDto;
+import com.example.shopofsoks.model.Sock;
+import com.example.shopofsoks.model.SockCount;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface SockMapper {
+
+    SockMapper INSTANCE = Mappers.getMapper(SockMapper.class);
+
+    @Mapping(source = "color", target = "color")
+    @Mapping(source = "cottonPart", target = "cottonPart")
+    Sock dtoToSock(SockDto sockDto);
+
+    @Mapping(source = "quantity", target = "count")
+    SockCount dtoToSockCount(SockDto sockDto);
+}
