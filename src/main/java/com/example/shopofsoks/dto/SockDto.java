20b78d0ef5fcebbc4f27a79b54f6415c3aaf9904
abdullinru <abdullin17021989@gmail.com>
2023-04-08 21:06:34 +0600

    написал юнит тесты для методов SockService

--- src/main/java/com/example/shopofsoks/dto/SockDto.java
+++ src/main/java/com/example/shopofsoks/dto/SockDto.java
@@ -1,9 +1,17 @@
 package com.example.shopofsoks.dto;
 
+import java.util.Objects;
+
 public class SockDto {
     private String color;
     private int cottonPart;
     private int quantity;
+
+    public SockDto(String color, int cottonPart, int quantity) {
+        this.color = color;
+        this.cottonPart = cottonPart;
+        this.quantity = quantity;
+    }
 
     public String getColor() {
         return color;
@@ -27,5 +35,25 @@ public class SockDto {
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SockDto sockDto = (SockDto) o;
+
+        if (cottonPart != sockDto.cottonPart) return false;
+        if (quantity != sockDto.quantity) return false;
+        return Objects.equals(color, sockDto.color);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = color != null ? color.hashCode() : 0;
+        result = 31 * result + cottonPart;
+        result = 31 * result + quantity;
+        return result;
     }
 }
---
+++ src/test/java/com/example/shopofsoks/service/SockServiceTest.java
@@ -0,0 +1,188 @@
+package com.example.shopofsoks.service;
+
+import com.example.shopofsoks.Operat;
+import com.example.shopofsoks.dto.SockDto;
+import com.example.shopofsoks.model.Sock;
+import com.example.shopofsoks.model.SockCount;
+import com.example.shopofsoks.repository.SockCountRepository;
+import com.example.shopofsoks.repository.SockRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+
+@ExtendWith(MockitoExtension.class)
+class SockServiceTest {
+    @Mock
+    private SockRepository sockRepository;
+    @Mock
+    private SockCountRepository sockCountRepository;
+
+    @InjectMocks
+    private SockService sockService;
+
+    Sock sock;
+    Sock sock2;
+    SockCount sockCount;
+    SockDto sockDto;
+    SockDto newSockDtoAfterIncome;
+    SockDto newSockDtoAfterOutcome;
+    SockDto wrongSockDto;
+
+    @BeforeEach
+    public void setUp(){
+        int id = 1;
+        String color = "Black";
+        int cottonPart = 10;
+        int wrongCottonPart = 110;
+        int quantity = 50;
+        sock = new Sock(id,color, cottonPart);
+        sock2 = new Sock(id,color, cottonPart);
+        sockCount = new SockCount(1, quantity);
+        sock.setSockCount(sockCount);
+        sock2.setSockCount(sockCount);
+        sockDto = new SockDto(color, cottonPart, quantity);
+        newSockDtoAfterIncome = new SockDto(color, cottonPart, 2*quantity);
+        newSockDtoAfterOutcome = new SockDto(color, cottonPart, 0);
+        wrongSockDto = new SockDto(color, wrongCottonPart, quantity);
+
+    }
+// Тесты метода по приходу носков на склад
+    @Test
+    public void incomeSockPositiveTestWhenFindSockNotNull() {
+
+        Mockito.when(sockRepository.findSocksByColorAndCottonPart(any(String.class), anyInt()))
+                .thenReturn(sock);
+        Assertions.assertThat(sockService.incomeSocks(sockDto)).isEqualTo(newSockDtoAfterIncome);
+    }
+    @Test
+    public void incomeSockPositiveTestWhenFindSockNull() {
+
+        Mockito.when(sockRepository.findSocksByColorAndCottonPart(any(String.class), anyInt()))
+                .thenReturn(null);
+        Assertions.assertThat(sockService.incomeSocks(sockDto)).isEqualTo(sockDto);
+    }
+    @Test
+    public void incomeSockNegativeTestWhenWrongParam() {
+
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> sockService.incomeSocks(wrongSockDto));
+    }
+    // Тесты метода по отпуску носков со склада
+    @Test
+    public void outcomeSockPositiveTestWhenFindSockNotNull() {
+
+        Mockito.when(sockRepository.findSocksByColorAndCottonPart(any(String.class), anyInt()))
+                .thenReturn(sock);
+        Assertions.assertThat(sockService.outcomeSocks(sockDto)).isEqualTo(newSockDtoAfterOutcome);
+    }
+    @Test
+    public void outcomeSockNegativeTestWhenFindSockNull() {
+
+        Mockito.when(sockRepository.findSocksByColorAndCottonPart(any(String.class), anyInt()))
+                .thenReturn(null);
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> sockService.outcomeSocks(sockDto));
+    }
+    @Test
+    public void outcomeSockNegativeTestWhenNotEnoughSocks() {
+
+        Mockito.when(sockRepository.findSocksByColorAndCottonPart(any(String.class), anyInt()))
+                .thenReturn(sock);
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> sockService.outcomeSocks(newSockDtoAfterIncome));
+    }
+    @Test
+    public void outcomeSockNegativeTestWhenWrongParam() {
+
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> sockService.outcomeSocks(wrongSockDto));
+    }
+
+    // Тесты метода по получению количества носков на складе по определенным параметрам
+    @Test
+    public void getCountTestWrongParam() {
+        String color = "Black";
+        int wrongCottonPart = 110;
+        Operat ravno = Operat.equal;
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> sockService.getCountSocksbyParam(color, ravno, wrongCottonPart ));
+    }
+
+    @Test
+    public void getCountOperMoreNegativeTest() {
+        String color = "Black";
+        int cottonPart = 10;
+        Operat more = Operat.moreThan;
+        Mockito.when(sockRepository.findSocksByColorAndCottonPartAfter(color, cottonPart))
+                .thenReturn(Collections.emptyList());
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> sockService.getCountSocksbyParam(color, more, cottonPart ));
+    }
+    @Test
+    public void getCountOperLessNegativeTest() {
+        String color = "Black";
+        int cottonPart = 10;
+        Operat less = Operat.lessThan;
+        Mockito.when(sockRepository.findSocksByColorAndCottonPartBefore(color, cottonPart))
+                .thenReturn(Collections.emptyList());
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> sockService.getCountSocksbyParam(color, less, cottonPart ));
+    }
+    @Test
+    public void getCountOperEqualNegativeTest() {
+        String color = "Black";
+        int cottonPart = 10;
+        Operat equal = Operat.equal;
+        Mockito.when(sockRepository.findSocksByColorAndCottonPart(color, cottonPart))
+                .thenReturn(null);
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> sockService.getCountSocksbyParam(color, equal, cottonPart ));
+    }
+
+    @Test
+    public void getCountOperEqualPositiveTest() {
+        String color = "Black";
+        int cottonPart = 10;
+        Operat equal = Operat.equal;
+        Mockito.when(sockRepository.findSocksByColorAndCottonPart(color, cottonPart))
+                .thenReturn(sock);
+        Assertions.assertThat(sockService.getCountSocksbyParam(color, equal, cottonPart)).isEqualTo(sock.getSockCount().getCount());
+    }
+    @Test
+    public void getCountOperMorePositiveTest() {
+        String color = "Black";
+        int cottonPart = 10;
+        Operat more = Operat.moreThan;
+        List<Sock> sockList = new ArrayList<>();
+        sockList.add(sock);
+        sockList.add(sock2);
+        Mockito.when(sockRepository.findSocksByColorAndCottonPartAfter(color, cottonPart))
+                .thenReturn(sockList);
+        Assertions.assertThat(sockService.getCountSocksbyParam(color, more, cottonPart)).isEqualTo(2*sock.getSockCount().getCount());
+    }
+    @Test
+    public void getCountOperLessPositiveTest() {
+        String color = "Black";
+        int cottonPart = 10;
+        Operat less = Operat.lessThan;
+        List<Sock> sockList = new ArrayList<>();
+        sockList.add(sock);
+        sockList.add(sock2);
+        Mockito.when(sockRepository.findSocksByColorAndCottonPartBefore(color, cottonPart))
+                .thenReturn(sockList);
+        Assertions.assertThat(sockService.getCountSocksbyParam(color, less, cottonPart)).isEqualTo(2*sock.getSockCount().getCount());
+    }
+
+}
